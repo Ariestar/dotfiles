913d42e5c09728fad600dc44c06bf186e0c16ef0
Ariestar <1941137088@qq.com>
2026-01-22 18:52:36 +0800

    Pr/update main (#13)
    * feat: 新增glazewm透明度显示
    
    * chore: 从git追踪中删除nushell history文件
    feat: glazewm因为性能原因改至100%
    
    * refactor: 删除nu欢迎banner
    feat: nu方向键补全历史记录提示

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 使用 Nu shell 作为终端
+vim.opt.shell = "nu"
